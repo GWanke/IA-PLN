a79d4e903450fa7797b8ecda5d7e50c7c7ee286b
Gwanke <ra91671@uem.br>
2020-11-25 12:06:41 -0300

    Stemming demorado

--- T2.py
+++ T2.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 import unidecode
 import pandas as pd
 import time
+from nltk.stem import PorterStemmer
 #import para a lista de stopwords do spacy
 import spacy
 #lista global de stopwords -- MEXER DEPOIS NELA, PARA ALTERAR OS STOPWORDS SENTIMENTAIS.
@@ -16,8 +17,8 @@ def readInput():
 	#panda dataframe
 	entrada = pd.read_csv('IMDB Dataset.csv')
 	#passa a entrada para uma serie, para o preprocessamento.
-	entrada=entrada['review'].apply(preProcessamento)
-	print(entrada.head())
+	entrada['review'] = entrada['review'].apply(preProcessamento)
+	print(entrada.head(10))
 
 def preProcessamento(linha):
 	#tira o HTML.
@@ -27,12 +28,15 @@ def preProcessamento(linha):
 	linha = unidecode.unidecode(linha)
 	#tira os pontos, transforma em minuscula
 	linha = re.sub('[^a-z\s]', '', linha.lower())
-	#separa em tokens. E possivel utilizar split pois foi usado um regex para tirar os pontos anteriormente. Alem disto, retira os stopWords.
-	linha = [palavra for palavra in linha.split() if palavra not in stopwords]
+	#stemming + tokenizer + Retira os stopWords.
+	ps = PorterStemmer()
+	linha = [ps.stem(palavra) for palavra in linha.split() if palavra not in stopwords]
 	#adiciona no dataframe.
 	return ','.join(linha)  
 
 def main():
+	if 'like' in stopwords:
+		print('ye')
 	start = time.time()
 	readInput()
 	end = time.time()
